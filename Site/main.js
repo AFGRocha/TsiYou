0688a568254431eef3296e13eacbc2995d5cfb5c
AFGRocha <antoniolaranjinha@hotmail.com>
2018-04-02 22:09:19 +0100

    Early Sign up code

--- Site/main.js
+++ Site/main.js
@@ -19,22 +19,46 @@ class User{
 
 function init(){
 
-    let usernamelogin = document.getElementById("username")
-    let passwordlogin = document.getElementById("password")
+    //Login
+    let usernameLogin = document.getElementById("username")
+    let passwordLogin = document.getElementById("password")
     let loginBtn = document.getElementById("login")
-    let email
+  
 
     loginBtn.addEventListener("click", function () {
 
         console.log("User Logged in")
-        console.log("Username: " + usernamelogin.value)
-        console.log("Password: " + passwordlogin.value)
+        console.log("Username: " + usernameLogin.value)
+        console.log("Password: " + passwordLogin.value)
 
         document.getElementById('logText').style.visibility = 'hidden';
         document.getElementById('signText').style.visibility = 'hidden';
-        document.getElementById('userText').innerHTML = usernamelogin.value + " " + '<i class="fa fa-user" style="font-size:24px"></i>';
+        document.getElementById('userText').innerHTML = usernameLogin.value + " " + '<i class="fa fa-user" style="font-size:24px"></i>';
         $('#loginModal').modal('hide');
         
     })
 
+
+
+    //SignUp
+    let email = document.getElementById("email")
+    let usernameSign = document.getElementById("newUser")
+    let passwordSign = document.getElementById("newPass")
+    let passwordSign2 = document.getElementById("newPass2")
+    let signupBtn = document.getElementById("signup")
+    let checker = document.getElementById("signCheck")
+
+    signupBtn.addEventListener("click", function () {
+
+        if (passwordSign.value == passwordSign2.value){
+            let myUser = new User(usernameSign.value,passwordSign.value,email.value)
+            console.log(myUser)
+            $('#signupModal').modal('hide');
+        }
+        else{
+            checker.innerHTML = "Passwords não estao iguais"
+        }
+
+
+    })
 }
